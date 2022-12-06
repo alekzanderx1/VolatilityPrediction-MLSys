eadce02fad1497f2f61cfa5b749ef83cd38f56c4
rahulb5 <rhlbhagtani81@gmail.com>
2022-12-05 23:49:54 -0500

    updating test flow

--- training_flow.py
+++ training_flow.py
@@ -55,13 +55,11 @@ class VolatilityPredictionFlow(FlowSpec):
         """
         Read the data in from the static file
         """
-        from io import StringIO
         import pandas as pd
         
-        self.dataframe = pd.read_csv(StringIO(self.DATA_FILE))
+        self.df = pd.read_csv("final.csv")
+        self.df.set_index('Date', inplace = True)
         
-        print("Total of {} rows in the dataset!".format(len(self.dataframe)))
-        # go to the next step
         self.next(self.check_dataset)
 
     @step
@@ -73,39 +71,68 @@ class VolatilityPredictionFlow(FlowSpec):
         # TODO Implement checks on Data Later
         assert(True)
         self.next(self.prepare_train_and_test_dataset)
+    
+    @step
+    def process_data(self) -> None:
+        """
+        Manipulate the dataframe to fill missing rows and split in two seperate dataframes
+        """
+
+        #creating two different dataframes for volatility and excess returns
+        self.vol_df = self.df.copy()
+        self.ret_df = self.df.copy()
+        
+        #Removing all the dates where we don't have volatility value
+        self.vol_df.dropna(subset = ['Volatility'], inplace = True)
+
+        #adding lagged variables based on exploratory data analysis
+        lagged_data = []
+        for i in range(1,10):
+            col_label = "Volshift" + str(i)
+            self.vol_df[col_label] = self.vol_df['Volatility'].shift(i)
+        
+        #forward fill all the rows
+        self.vol_df.ffill(inplace = True)
+
+        #drop all the rows with NA
+        self.vol_df.dropna(inplace = True)
+
+        #Removing all the dates where we don't have excess return value
+        self.ret_df.dropna(subset = ['Mkt_rf'], inplace = True)
+
+        #adding lagged variables based on 
+        self.ret_df['Mkt_rf_shifted'] = self.ret_df['Mkt_df'].shift(1)
+
+        #forward fill all the rows
+        self.ret_df.ffill(inplace = True)
+
+        #drop all the rows with NA
+        self.ret_df.dropna(inplace = True)
+
+        #split the dataframes in X and y dataframes
+        vol_col = [x for x in self.vol_df.keys() if x != 'Volatility']
+        self.vol_df_X = self.vol_df[vol_col]
+        self.vol_df_y = self.vol_df['Volatility']
+
+        ret_col = [x for x in self.ret_df.keys() if x != 'Mkt_rf']
+        self.ret_df_X = self.ret_df[ret_col]
+        self.ret_df_y = self.ret_df['Mkt_rf']
+
+        self.next(self.train_test_split)
 
     @step
-    def prepare_train_and_test_dataset(self):
+    def train_test_split(self):
         import pandas as pd
         
-        def value_to_float(x):
-            if type(x) == float or type(x) == int:
-                return x
-            if 'M' in x:
-                if len(x) > 1:
-                    return float(x.replace('M', '')) * 1000000
-                return 1000000.0
-            return 0.0
+        len_train_vol = round(len(self.vol_df)*0.7)
+        len_train_ret = round(len(self.ret_df)*0.7)
         
-        # Set Date as Index
-        self.dataframe.Date = pd.to_datetime(self.dataframe.Date)
-        self.dataframe = self.dataframe.set_index('Date')
-        self.dataframe = self.dataframe.sort_index()
-        # Shift Volatility forward by 1 week as this the the target we will be predicting
-        self.dataframe['Volatility'] = self.dataframe.Volatility.shift(1)
-        self.dataframe = self.dataframe.iloc[1: , :]
-        # Move target column to end
-        volatility_column = self.dataframe.pop('Volatility')
-        self.dataframe.insert(len(self.dataframe.columns),"Volatility",volatility_column)
-        # TODO impute NaN values
-        self.dataframe = self.dataframe.dropna()
-        # Convert Mcap to float
-        self.dataframe['Mcap'] = self.dataframe['Mcap'].apply(value_to_float) 
+        self.vol_train_X, self.vol_test_X = self.vol_df_X[:len_train_vol], self.vol_df_X[len_train_vol:]
+        self.vol_train_y, self.vol_test_y = self.vol_df_y[:len_train_vol], self.vol_df_y[len_train_vol:]
         
-        # TODO scale the values in each column
-        
-        # spilit into train and test sets
-        self.train, self.test = self.dataframe[0:1600], self.dataframe[1600:]
+        self.ret_train_X, self.ret_test_X = self.ret_df_X[:len_train_ret], self.ret_df_X[len_train_ret:]
+        self.ret_train_y, self.ret_test_y = self.ret_df_y[:len_train_ret], self.ret_df_y[len_train_ret:]
+
         self.next(self.train_walk_forward_validation)
         
 
